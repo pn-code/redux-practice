93eed8a656c63eda79a7a35e5bd02bc1c959460d
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-07 14:33:45 -0800

    Added AddPostForm to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 // import Counter from "./features/counter/Counter";
 import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
 
 function App() {
     return (
         <div className="App">
+            <AddPostForm/>
             <PostsList/>
         </div>
     );
