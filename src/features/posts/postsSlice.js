b6aa5e8ef6c2141c472ab9ec6148559f442bebd3
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-07 13:59:08 -0800

    Export selectAllPosts and postAdded reducer action

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -5,17 +5,31 @@ import { createSlice } from "@reduxjs/toolkit";
 
 // Step 2: create initialState
 const initialState = [
-    {id: '1', title: "Learning Redux Toolkit", content: "State Management is important"},
-    {id: '2', title: "About Slices", content: "I can create a slice!"},
-]
+    {
+        id: "1",
+        title: "Learning Redux Toolkit",
+        content: "State Management is important",
+    },
+    { id: "2", title: "About Slices", content: "I can create a slice!" },
+];
 
 // Step 3: Create your slice
 const postsSlice = createSlice({
-    name: 'posts',
+    name: "posts",
     initialState,
     reducers: {
-    }
-})
+        postAdded: (state, action) => {
+            // Payload references form data we will use later.
+            state.push(action.payload);
+        },
+    },
+});
+
+// Selects all posts in the slice
+export const selectAllPosts = (state) => state.posts;
+
+// Export reducer actions
+export const { postAdded } = postsSlice.actions;
 
 // Step 4: export slice.reducer as default
-export default postsSlice.reducer
+export default postsSlice.reducer;
