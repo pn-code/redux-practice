8542f4c4d44fef6ba4515ab1b4a5fa05fbd1fab2
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-07 15:45:37 -0800

    Built out TimeAgo to display timestamp of posts

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -2,18 +2,23 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 
 const PostsList = () => {
     // After creating component, useSelector to reference global state like so.
     const posts = useSelector(selectAllPosts);
 
+    // Filter method to order posts
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+
     // In this case, map through posts and render information in desired format
-    const renderedPosts = posts.map((post) => (
+    const renderedPosts = orderedPosts.map((post) => (
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content.substring(0, 100)}</p>
             <p>
-                <PostAuthor userId={post.userId}/>
+                <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
             </p>
         </article>
     ));
