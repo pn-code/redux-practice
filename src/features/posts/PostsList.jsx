8e0c420f6d745ec23cf8d096a220791990903c27
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-07 13:59:34 -0800

    Built out PostsList component

---
+++ src/features/posts/PostsList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectAllPosts } from "./postsSlice";
+
+const PostsList = () => {
+    // After creating component, useSelector to reference global state like so.
+    const posts = useSelector(selectAllPosts);
+
+    // In this case, map through posts and render information in desired format
+    const renderedPosts = posts.map((post) => (
+        <article key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content.substring(0, 100)}</p>
+        </article>
+    ));
+
+    return (
+        <div>
+            <h2>Posts</h2>
+            {renderedPosts}
+        </div>
+    );
+};
+
+export default PostsList;
