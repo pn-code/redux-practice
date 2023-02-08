fa6126be91eaf1e0d4ec46adb69f2b5b8adc248d
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-07 20:44:36 -0800

    Added immediate fetching of users when app loads

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { fetchUsers } from "./features/users/usersSlice";
+
+// Immediately fetches users when the app loads...
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
